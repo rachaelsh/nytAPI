60204600e1e3636d1b5d609178e171e656874d72
Rachael <rachael.s.harlow@gmail.com>
2017-02-14 11:14:45 -0700

    beginning front end

---
+++ app.js
@@ -0,0 +1,47 @@
+// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+// url += '?' + $.param({
+//   'api-key': "8eb2dc46813c4e2ab0c2ef02ee9f0fa6",
+//   'q': "chicken"
+// });
+// $.ajax({
+//   url: url,
+//   method: 'GET',
+// }).done(function(result) {
+//   console.log(result);
+// }).fail(function(err) {
+//   throw err;
+// });
+// //use angular
+//
+//
+//
+//
+// angular.module("trelloApp").service("mainServ", function($http){
+//   this.loginUser = function(user){
+//     return $http({
+//       method: "POST",
+//       url: "/login",
+//       data: user
+//     }).then(function(response){
+//       return response.data;
+//     })
+//   };
+//   this.getCurrentUser = function(user){
+//     return $http({
+//       method: "GET",
+//       url: "/current"
+//     }).then(function(response){
+//       return response.data;
+//     });
+//   };
+// });
+
+
+
+
+
+
+
+
+// 9. Create a function called evenOdd that takes in the array and pushes into a new array "even" or "odd" for every value in the array, depending upon whether they are even or odd number values. Return the new array.
+// Hint: Use the modulus operator
---
+++ images/NYTimeless _ History on the Front Page_files/mainCtrl.js.download
@@ -0,0 +1,25 @@
+var app = angular.module("nytApp", []);
+app.controller("mainCtrl", function($scope){
+$scope.hello = "hi there";
+var articles = [
+  {
+    "year": 1984,
+    "topic": "Suffrage",
+    "person": "Eleanor Roosevelt",
+    "vintage": "true"
+  },
+  {
+    "year": 1984,
+    "topic": "Suffrage",
+    "person": "Eleanor Roosevelt",
+    "vintage": "true"
+  },
+  {
+    "year": 1984,
+    "topic": "Suffrage",
+    "person": "Eleanor Roosevelt",
+    "vintage": "true"
+  }
+];
+$scope.articles = articles;
+});
