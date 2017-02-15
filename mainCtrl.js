96ccdaba5a2df088f8d2246dc9230dd1147cc70d
Rachael <rachael.s.harlow@gmail.com>
2017-02-15 00:20:09 -0700

    api almost working

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,25 +1,44 @@
 var app = angular.module("nytApp", []);
-app.controller("mainCtrl", function($scope){
-$scope.hello = "hi there";
-var articles = [
-  {
-    "year": 1984,
-    "topic": "Suffrage",
-    "person": "Eleanor Roosevelt",
-    "vintage": "true"
-  },
-  {
-    "year": 1984,
-    "topic": "Suffrage",
-    "person": "Eleanor Roosevelt",
-    "vintage": "true"
-  },
-  {
-    "year": 1984,
-    "topic": "Suffrage",
-    "person": "Eleanor Roosevelt",
-    "vintage": "true"
-  }
-];
-$scope.articles = articles;
-});
+app.controller("mainCtrl", function($scope, $http, $httpParamSerializerJQLike){
+
+
+$scope.searchArticles = function(input){
+  console.log(input);
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': "8eb2dc46813c4e2ab0c2ef02ee9f0fa6",
+      'q': input + "",
+        'end_date': "19691231"
+  }, true);
+
+
+
+$http.get(url).success(function(response){
+    $scope.articles = response.response.docs;
+      console.log(response);
+    // console.log(response.response.docs[0].multimedia[0].url);
+  });
+};
+
+// $scope.favoriteArticle = function(article){
+//   console.log(article);
+// }
+// $scope.removeArticle = function(article){
+// var index = $scope.articles.indexOf(article);
+//   $scope.articles.splice(index, 1);
+// }
+
+
+//add article (use this to display search terms you selected?) > connect with Edit
+// $scope.addParameters = function(param){
+// console.log(param);
+// }
+//
+
+
+
+
+
+
+
+});//end of controller
